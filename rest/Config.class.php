9bf3dab4867fb3dd465ace9db5294319ec5e6d56
AjdinBajric <bajric.ajdin@gmail.com>
2023-03-18 12:21:16 +0100

    finished login and register

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -1,24 +1,42 @@
 <?php
 
-class Config{
+class Config
+{
 
-    public static function DB_HOST(){
+    public static function DB_HOST()
+    {
         return Config::get_env("DB_HOST", "127.0.0.1");
     }
-    public static function DB_USERNAME(){
+
+    public static function DB_USERNAME()
+    {
         return Config::get_env("DB_USERNAME", "root");
     }
-    public static function DB_PASSWORD(){
+
+    public static function DB_PASSWORD()
+    {
         return Config::get_env("DB_PASSWORD", "");
     }
-    public static function DB_SCHEMA(){
+
+    public static function DB_SCHEMA()
+    {
         return Config::get_env("DB_SCHEMA", "tba");
     }
-    public static function DB_PORT(){
+
+    public static function DB_PORT()
+    {
         return Config::get_env("DB_PORT", "3306");
     }
-    public static function get_env($name, $default){
+
+    public static function JWT_SECRET(){
+        return Config::get_env("JWT_SECRET", "guesswhatthisisjwtsecret");
+    }
+
+    public static function get_env($name, $default)
+    {
         return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
     }
+
 }
+
 ?>
--- rest/routes/UserRoutes.php
+++ rest/routes/UserRoutes.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../Config.class.php';
 
+use Firebase\JWT\JWT;
+
 Flight::route('GET /username', function () {
     $valid_user = Flight::get('validUser');
     Flight::json($valid_user['username']);
@@ -16,55 +18,65 @@ Flight::route('GET /firstname', function () {
     Flight::json($valid_user['firstname']);
 });
 
-Flight::route('POST /register', function () {
-    $registered_user = Flight::request()->data->getData();
-    $storedUser = Flight::userService()->get_user_by_username($registered_user['username']);
-
-    if (isset($storedUser['id'])) {
-        Flight::json(["message" => "User with that username already exists. Try different username."], 404); #check return code
-    } else {
-        Flight::json(Flight::userService()->add_element($registered_user));
-    }
-});
-
-Flight::route('POST /login', function () {
-    $login_user_data = Flight::request()->data->getData();
-    $user = Flight::userService()->get_user_by_username($login_user_data['username']);
-
-    if (isset($user['id'])) {
-        if ($user['password'] == $login_user_data['password']) {
-            unset($user['password']);
-            $jwt = JWT::encode($user, Config::JWT_SECRET(), 'HS256');
-            Flight::json(['token' => $jwt]);
-        } else {
-            Flight::json(["message" => "Invalid credentials."], 404);
-        }
-    } else {
-        Flight::json(["message" => "User does not exist."], 404);
-    }
-});
-
-Flight::route('GET /user', function(){
+Flight::route('GET /user', function () {
     Flight::json(Flight::userService()->get_all());
 });
 
-Flight::route('POST /user', function (){
+Flight::route('POST /user', function () {
     $new_user = Flight::request()->data->getData();
     Flight::json(Flight::userService()->add_element($new_user));
 });
 
-Flight::route('GET /user/@id', function ($id){
+Flight::route('GET /user/@id', function ($id) {
     Flight::json(Flight::userService()->get_by_id($id));
 });
 
-Flight::route('DELETE /user/@id', function ($id){
+Flight::route('DELETE /user/@id', function ($id) {
     Flight::userService()->delete_element($id);
     Flight::json(["message" => "User deleted."]);
 });
 
-Flight::route('PUT /user/@id', function ($id){
+Flight::route('PUT /user/@id', function ($id) {
     $new_data = Flight::request()->data->getData();
     Flight::json(Flight::userService()->update_element($id, $new_data));
+});
+
+
+Flight::route('POST /register', function () {
+    $registerUser = Flight::request()->data->getData();
+    $storedUser = Flight::userService()->get_user_by_username($registerUser['username']);
+
+    if (isset($storedUser['id'])) {
+        Flight::json(["message" => "User with that username already exists. Try different username."], 404);
+    } else {
+        $options = [
+            'cost' => 10 // The higher the cost, the more secure the hash (but also slower to compute)
+        ];
+        $registerUser['password'] = password_hash($registerUser['password'], PASSWORD_DEFAULT, $options);
+        Flight::userService()->add_element($registerUser);
+        Flight::json(["success" => "true"]);
+    }
+});
+
+
+Flight::route('POST /login', function () {
+
+    $login = Flight::request()->data->getData();
+
+    $user = Flight::userService()->get_user_by_username($login['username']);
+
+    if (isset($user['id'])) {
+
+        if (password_verify($login['password'], $user['password'])) {
+            unset($user['password']);
+            $jwt = JWT::encode($user, Config::JWT_SECRET(), 'HS256');
+            Flight::json(['token' => $jwt]);
+        } else {
+            Flight::json(["message" => "Password is incorrect"], 404);
+        }
+    } else {
+        Flight::json(["message" => "User with that username doesn't exist"], 404);
+    }
 });
 
 ?>
